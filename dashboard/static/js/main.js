8eb6723c53856093973542da21e61500953c2a91
Lucas Flender <lu851fle@htwg-konstanz.de>
2022-05-24 21:03:55 +0200

    added data reception from aws api

--- dashboard/static/js/main.js
+++ dashboard/static/js/main.js
@@ -1,5 +1,9 @@
-
+const refreshInterval = 2000
+const dataApiUrl = 'https://zuyvro6601.execute-api.eu-central-1.amazonaws.com/default/IIoTLambdaApiMicroservice';
+const sensorsApiUrl = '';
+let lastRecievedTimestamp = 0;
 let chart;
+
 window.addEventListener('load', function () {
     chart = new Highcharts.Chart({
         chart: {
@@ -64,127 +68,49 @@ window.addEventListener('load', function () {
     });
 });
 
-// chart = Highcharts.chart('container1', {
-
-//     title: {
-//         text: 'Line chart'
-//     },
-
-//     subtitle: {
-//         text: 'subtitle'
-//     },
-
-//     yAxis: {
-//         title: {
-//             text: 'Value'
-//         }
-//     },
-
-//     xAxis: {
-//         categories: []
-//     },
-
-//     legend: {
-//         layout: 'vertical',
-//         align: 'right',
-//         verticalAlign: 'middle'
-//     },
-
-//     plotOptions: {
-//         series: {
-//             label: {
-//                 connectorAllowed: false
-//             }
-//         }
-//     },
-//     series: [{
-//         name: 'Humdity',
-//         data: []
-//     }, {
-//         name: 'Temperature',
-//         data: []
-//     }],
-
-//     responsive: {
-//         rules: [{
-//             condition: {
-//                 maxWidth: 500
-//             },
-//             chartOptions: {
-//                 legend: {
-//                     layout: 'horizontal',
-//                     align: 'center',
-//                     verticalAlign: 'bottom'
-//                 }
-//             }
-//         }]
-//     }
-// });
-
-
 /**
 * Request data from the server, add it to the graph and set a timeout to request again
 */
 async function requestData() {
-    const result = await fetch('https://demo-live-data.highcharts.com/time-rows.json');
+    let url = new URL(dataApiUrl);
+    let params = {device_id: "1", start_timestamp: lastRecievedTimestamp};
+    url.search = new URLSearchParams(params).toString();
+
+    const result = await fetch(url);
     if (result.ok) {
         const data = await result.json();
-        const [date, value] = data[0];
-        const point = [new Date(date).getTime(), value * 10];
-        const series = chart.series[0],
-            shift = series.data.length > 20; // shift if the series is longer than 20
-        // add the point
-        chart.series[0].addPoint(point, true, shift);
+        const plotData = await transformData(data);
+        // get last element of the timestamp array plotData[0] = [ts1, ts2, ..., tsn]
+        // update lastRecievedTimestamp if array has length of more than 0
+        lastRecievedTimestamp = plotData[0].length ? plotData[0][plotData[0].length - 1]/1000 : lastRecievedTimestamp; 
+        console.log(plotData, "\nNew lasrRcTs: ", lastRecievedTimestamp);
 
-        const point2 = [new Date(date).getTime(), value * 15];
-        const series2 = chart.series[1],
-            shift2 = series2.data.length > 20; // shift if the series is longer than 20
-        // add the point
-        chart.series[1].addPoint(point2, true, shift2);
+        for (const [idx, ts] of plotData[0].entries()) {
+            const point = [ts, plotData[1][idx]];
+            const series = chart.series[0],
+                shift = series.data.length > 20; // shift if the series is longer than 20
+            // add the point
+            chart.series[0].addPoint(point, true, shift);
+
+            const point2 = [ts, plotData[2][idx]];
+            const series2 = chart.series[1],
+                shift2 = series2.data.length > 20; // shift if the series is longer than 20
+            // add the point
+            chart.series[1].addPoint(point2, true, shift2);
+        }
         // call it again after one second
-        setTimeout(requestData, 5000);
+        setTimeout(requestData, refreshInterval);
     }
 }
 
-setTimeout(async () => {
-    const result = await fetch('https://zuyvro6601.execute-api.eu-central-1.amazonaws.com/default/IIoTLambdaApiMicroservice?TableName=IIoTDataStorag');
-    console.log(result.json());
-}, 5000)
-
-// let getData = function () {
-//     // $.ajax({
-//     //     type: "GET",
-//     //     dataType: "json",
-//     //     async: false,
-//     //     success: function (data) {
-//     //         console.log('data', data);
-//     //         drawChart(data);
-//     //     },
-//     //     error: function (xhr, status, error) {
-//     //         console.error("JSON error: " + status);
-//     //     }
-//     // });
-//     drawChart(null);
-// }
-
-// let drawChart = function (data) {
-
-//     humArr = [1, 2, 3, 4, 5, 4, 3, 2, 1];
-//     tempArr = [2, 4, 5, 6, 5, 4, 3, 2, 1];
-//     upArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-//     myChart.series[0].setData(humArr, true)
-//     myChart.series[1].setData(tempArr, true)
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log("DOM Loaded, Getting Chart Data")
-//     getData();
-// });
-
-// const refreshInterval = 5 * 1000; // refresh every 5000 ms
-// setInterval(() => {
-//     console.log("Refreshing Chart Data")
-//     getData();
-// }, refreshInterval);
-
+async function transformData(data) {
+    const plotData = data.Items.reduce((acc, item) => {
+        if (item.timestamp){
+            acc[0].push(new Date(item.timestamp * 1000).getTime())
+            acc[1].push(isNaN(item.temperature) ? null : item.temperature)
+            acc[2].push(isNaN(item.humidity) ? null : item.humidity)
+        }
+        return acc;
+    }, [[],[],[]]); // initialize array of arrays for arr[0] => timestamps, arr[1] => temperature, arr[2] => humidity
+    return plotData;
+}
