fe964d46fe1197d2cebc7c7a68596aa5666c6d87
Christoph Casagranda <chriscasagranda@web.de>
2022-05-30 19:36:26 +0200

    Change ID to Device ID

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ pip3 install awsiotsdk
 pip3 install awscrt
 pip3 install awsiot
 
-python3 ./aws-iot/test.py -e a27oynsxnin6nw-ats.iot.eu-central-1.amazonaws.com -r ./aws-iot/keys/root-CA.crt -c ./aws-iot/keys/certificate.crt -k ./aws-iot/keys/private.key -id basicPubSub
+python3 ./aws-iot/test.py -e a27oynsxnin6nw-ats.iot.eu-central-1.amazonaws.com -r ./aws-iot/keys/root-CA.crt -c ./aws-iot/keys/certificate.crt -k ./aws-iot/keys/private.key
