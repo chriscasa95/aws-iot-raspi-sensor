85984762bffbaa4dbee177ca3ce81985bcc66874
Christoph Casagranda <chriscasagranda@web.de>
2022-05-30 20:32:53 +0200

    Update Location names

--- dashboard/static/js/data.js
+++ dashboard/static/js/data.js
@@ -1,33 +1,35 @@
-export const dataApiUrl = 'https://zuyvro6601.execute-api.eu-central-1.amazonaws.com/default/IIoTLambdaApiMicroservice';
+export const dataApiUrl =
+	'https://zuyvro6601.execute-api.eu-central-1.amazonaws.com/default/IIoTLambdaApiMicroservice';
 
-export const availableDevices = ['C3', 'HTWG', 'Uni']
+export const availableDevices = ['C3', 'HTWG', 'Schaenzlebruecke', 'Zaehringerplatz'];
 
 export async function getData(currentDeviceId, lastRecievedTimestamp) {
-    let url = new URL(dataApiUrl);
-    let params = { device_id: currentDeviceId, start_timestamp: lastRecievedTimestamp };
-    url.search = new URLSearchParams(params).toString();
+	let url = new URL(dataApiUrl);
+	let params = { device_id: currentDeviceId, start_timestamp: lastRecievedTimestamp };
+	url.search = new URLSearchParams(params).toString();
 
-    let data = { Items: [] };
-    const result = await fetch(url);
-    if (result.ok) {
-        data = await result.json();
-    }
-    return data;
+	let data = { Items: [] };
+	const result = await fetch(url);
+	if (result.ok) {
+		data = await result.json();
+	}
+	return data;
 }
 
 export async function transformData(data, limit) {
-    console.log(data)
-    const plotData = data.Items
-    .slice(-limit)
-    .reduce((acc, item) => {
-        if (item.timestamp) {
-            acc[0].push(new Date(item.timestamp * 1000).getTime());
-            acc[1].push(isNaN(item.temperature) ? null : item.temperature);
-            acc[2].push(isNaN(item.humidity) ? null : item.humidity);
-            acc[3].push(isNaN(item.pm2_5) ? null : item.pm2_5);
-            acc[4].push(isNaN(item.pm10) ? null : item.pm10);
-        }
-        return acc;
-    }, [[], [], [], [], []]); // initialize array of arrays for arr[0] => timestamps, arr[1] => temperature, arr[2] => humidity
-    return plotData;
+	console.log(data);
+	const plotData = data.Items.slice(-limit).reduce(
+		(acc, item) => {
+			if (item.timestamp) {
+				acc[0].push(new Date(item.timestamp * 1000).getTime());
+				acc[1].push(isNaN(item.temperature) ? null : item.temperature);
+				acc[2].push(isNaN(item.humidity) ? null : item.humidity);
+				acc[3].push(isNaN(item.pm2_5) ? null : item.pm2_5);
+				acc[4].push(isNaN(item.pm10) ? null : item.pm10);
+			}
+			return acc;
+		},
+		[[], [], [], [], []]
+	); // initialize array of arrays for arr[0] => timestamps, arr[1] => temperature, arr[2] => humidity
+	return plotData;
 }
