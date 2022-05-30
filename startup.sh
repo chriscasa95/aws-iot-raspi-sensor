9e7652b930febb259c55548e56b31a92587e2f15
Christoph Casagranda <chriscasagranda@web.de>
2022-05-30 10:51:19 +0200

    Bypass Raspi awscrt issue

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-pipenv install
-pipenv run python ./aws-iot/test.py -e a27oynsxnin6nw-ats.iot.eu-central-1.amazonaws.com -r ./aws-iot/keys/root-CA.crt -c ./aws-iot/keys/test_thing.cert.pem -k ./aws-iot/keys/test_thing.private.key -id basicPubSub
+pip3 install gpiozero
+pip3 install awsiotsdk
+pip3 install awscrt
+pip3 install awsiot
+
+python3 ./aws-iot/test.py -e a27oynsxnin6nw-ats.iot.eu-central-1.amazonaws.com -r ./aws-iot/keys/root-CA.crt -c ./aws-iot/keys/test_thing.cert.pem -k ./aws-iot/keys/test_thing.private.key -id basicPubSub
