5fda020e6ba327898ec1e774674fd0c403b890ca
Christoph Casagranda <chriscasagranda@web.de>
2022-05-30 15:46:45 +0200

    Add table to map

--- dashboard/static/js/map.js
+++ dashboard/static/js/map.js
@@ -1,8 +1,10 @@
 import { availableDevices, getData, transformData } from './data.js';
 
-let lastRecievedTimestamp = 0;
+let lastRecievedTimestamp = Math.round(new Date().getTime() / 1000) - 60 * 60 * 24;
 
 var map = L.map('map').setView([47.679006, 9.162831], 14);
+
+var last_marker = null;
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
@@ -12,6 +14,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const dataModal = new bootstrap.Modal(document.getElementById('dataModal'), {
 	keyboard: false,
 });
+
+document.getElementById('dataModelButton').addEventListener('click', buttonClicked);
 
 async function init() {
 	for (const device in availableDevices) {
@@ -23,32 +27,65 @@ async function init() {
 			const long = data.Items[0].long;
 			const device_id = data.Items[0].device_id;
 
-			L.marker([lat, long], { device_id: device_id })
+			L.marker([long, lat], { device_id: device_id })
 				.addTo(map)
-				.on('mouseover', on_mouseover)
+				// .on('mouseover', on_mouseover)
 				.on('click', on_click);
 		}
 	}
 }
 
-async function on_mouseover(e) {
-	console.log(e);
+async function on_click(e) {
 	const data = await getData(e.target.options.device_id, lastRecievedTimestamp);
 	const plotData = await transformData(data);
 
 	console.log(plotData);
-	dataModal.show();
-	document.getElementById('dataModal').style.marginTop = e.containerPoint.y - 50;
+
+	document.getElementById('dataModal').style.marginTop = e.containerPoint.y - 200;
 	document.getElementById('dataModal').style.marginLeft = e.containerPoint.x - 320;
-	console.log(document.getElementById('dataModal').style);
 
 	document.getElementById('dataModalHeader').innerHTML = e.target.options.device_id;
-	document.getElementById('dataModalBody').innerHTML = e.target.options.device_id;
+
+	document.getElementById('dataModalBody').innerHTML = `<table class="table">
+	<thead>
+	  <tr>
+		<th scope="col">#</th>
+		<th scope="col">Name</th>
+		<th scope="col">Value</th>
+	  </tr>
+	</thead>
+	<tbody>
+	  <tr>
+		<th scope="row">1</th>
+		<td>Temperature</td>
+		<td>${plotData[1].slice(-1)}</td>
+	  </tr>
+	  <tr>
+		<th scope="row">2</th>
+		<td>Humidity</td>
+		<td>${plotData[2].slice(-1)}</td>
+	  </tr>
+	  <tr>
+		<th scope="row">3</th>
+		<td>PM10</td>
+		<td>${plotData[4].slice(-1)}</td>
+	  </tr>
+	  <tr>
+		<th scope="row">4</th>
+		<td>PM2.5</td>
+		<td>${plotData[3].slice(-1)}</td>
+
+	</tr>
+	</tbody>
+  </table>`;
+	console.log(dataModal._isShown);
+	dataModal.show();
+
+	last_marker = e;
 }
 
-async function on_click(e) {
-	// console.log(e.target.options);
-	window.location.href = `/dashboard/static/index.html?device_id=${e.target.options.device_id}`;
+async function buttonClicked() {
+	window.location.href = `/dashboard/static/index.html?device_id=${last_marker.target.options.device_id}`;
 }
 
 init();
