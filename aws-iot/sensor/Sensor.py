3686e91ebbd226639846aa81baaaf0fe15c12000
Christoph Casagranda <chriscasagranda@web.de>
2022-05-30 17:11:27 +0200

    Add time dependent randomness

--- aws-iot/sensor/Sensor.py
+++ aws-iot/sensor/Sensor.py
@@ -1,7 +1,8 @@
-from operator import imod
 import time
 import random
 import json
+from datetime import datetime
+from pytz import timezone
 
 from dataclasses import dataclass
 
@@ -31,27 +32,31 @@ class Sensor:
         self.__long = long
         self.__device_id = device_id
 
+        env = self.__enviroment()
+
         # start values
-        self.__temp = 20
-        self.__humidity = 40
-        self.__pm2_5 = 15
-        self.__pm10 = 20
+        self.__temp = float(random.randint(env[0][0], env[0][1]))
+        self.__humidity = float(random.randint(env[1][0], env[1][1]))
+        self.__pm2_5 = float(random.randint(env[2][0], env[2][1]))
+        self.__pm10 = float(random.randint(env[3][0], env[3][1]))
 
     def measure(self) -> SensorData:
+        env = self.__enviroment()
+
         self.__temp = self.__random_walk(
-            start=10, stop=35, decimals=1, dx=0.2, x=self.__temp
+            start=env[0][0], stop=env[0][1], decimals=1, dx=env[0][2], x=self.__temp
         )
 
         self.__humidity = self.__random_walk(
-            start=10, stop=35, decimals=1, dx=0.2, x=self.__humidity
+            start=env[1][0], stop=env[1][1], decimals=1, dx=env[1][2], x=self.__humidity
         )
 
         self.__pm2_5 = self.__random_walk(
-            start=10, stop=25, decimals=1, dx=0.1, x=self.__pm2_5
+            start=env[2][0], stop=env[2][1], decimals=1, dx=env[2][2], x=self.__pm2_5
         )
 
         self.__pm10 = self.__random_walk(
-            start=5, stop=50, decimals=1, dx=0.2, x=self.__pm10
+            start=env[3][0], stop=env[3][1], decimals=1, dx=env[3][2], x=self.__pm10
         )
 
         timestamp = int(time.time())
@@ -79,6 +84,19 @@ class Sensor:
             value = x + random.uniform(-dx, dx)
 
         return round(value, decimals)
+
+    def __enviroment(self):
+
+        gmt = timezone("Europe/Stockholm")
+
+        hour = datetime.now(gmt).hour
+
+        if hour >= 22 or hour <= 5:
+            return [(5, 20, 0.2), (20, 100, 1), (0, 10, 2), (0, 20, 5)]
+        elif (hour >= 6 and hour <= 9) or (hour >= 19 and hour <= 21):
+            return [(10, 25, 0.2), (20, 100, 1), (5, 20, 2), (10, 50, 5)]
+        elif hour >= 10 and hour <= 18:
+            return [(20, 35, 0.2), (20, 100, 1), (5, 20, 2), (10, 40, 5)]
 
 
 if __name__ == "__main__":
