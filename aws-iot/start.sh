277657826c66a667d3810e39d02569c1b9c5def4
Christoph Casagranda <chriscasagranda@web.de>
2022-05-31 08:47:55 +0200

    Cleanup

--- aws-iot/start.sh
+++ aws-iot/start.sh
@@ -11,5 +11,5 @@ export DEVICE_ID="Schaenzlebruecke"
 export LAT="47.673468"
 export LONG="9.157934"
 
-python ./test.py -e a27oynsxnin6nw-ats.iot.eu-central-1.amazonaws.com -r keys/root-CA.crt -c keys/certificate.crt -k keys/private.key
+python ./main.py -e a27oynsxnin6nw-ats.iot.eu-central-1.amazonaws.com -r keys/root-CA.crt -c keys/certificate.crt -k keys/private.key
 
--- sensor.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from gpiozero import CPUTemperature
-from time import sleep
-
-while True:
-    cpu = CPUTemperature()
-    print(cpu.temperature)
-
-    sleep(1)
